5460736610bbd257cb118e5c7ea88070a5b5aa74
ShaloTou <interamrez92@gmail.com>
2021-06-30 14:20:21 +0300

    components stacture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PreloadComponent } from './components/preload/preload.component';
+import { HomeComponent } from './components/screens/home/home.component';
+import { ServicesComponent } from './components/screens/services/services.component';
+import { AboutComponent } from './components/screens/about/about.component';
+import { HowWorksComponent } from './components/screens/how-works/how-works.component';
+import { DeliveryComponent } from './components/screens/delivery/delivery.component';
+import { TestimonialsComponent } from './components/screens/testimonials/testimonials.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PreloadComponent,
+    HomeComponent,
+    ServicesComponent,
+    AboutComponent,
+    HowWorksComponent,
+    DeliveryComponent,
+    TestimonialsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
